7d7a239aa5dd3e3fb0f157abdfc511d2829def9a
Naila <98486996+nailasuely@users.noreply.github.com>
2024-12-22 13:22:27 -0300

    ✨ feat: Funções para conversão de massa adicionadas

---
+++ features/massa/massa.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+double quiloParaGrama(double quilo) {
+    return quilo * 1000;
+}
+
+double quiloParaTonelada(double quilo) {
+    return quilo / 1000;
+}
+
+double gramaParaQuilo(double grama) {
+    return grama / 1000;
+}
+
+double toneladaParaQuilo(double tonelada) {
+    return tonelada * 1000;
+}
+
